71faaf95eff4af606b43cd7e6a7b57883752352d
Vinay Vijayan <informvinay@gmail.com>
2021-06-01 22:16:53 -0400

    Commit message in quotes

---
+++ test.java
@@ -0,0 +1,9 @@
+class test {
+
+  public static void main(String args[]){
+    System.out.println(" Testing this is cool");
+    System.out.println(" This is something");
+    System.out.println(" This is something");
+  }
+
+}
