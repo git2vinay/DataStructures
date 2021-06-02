e5dc7e7c3413912bc89e3b1b76afbb607c736988
Your Name <informvinay@gmail.com>
2021-06-01 23:03:37 -0400

    Testing command line commit message

--- Sample.java
+++ Sample.java
@@ -1,9 +1,13 @@
+import java.util.*;
 class Sample
 {
   public static void main(String args[]){
 
       System.out.println(" this is good");
       System.out.println(" this is good,edited from the git");
+      LinkedList<String> list=new LinkedList<String>();
+      list.add("Test");
+      System.out.println("Linked List"+list);
   }
 
 }
