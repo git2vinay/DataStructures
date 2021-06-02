c92c578ecf2bc87d8786b0268a81da24dcdfcf83
vkv <vinay>
2021-06-02 18:15:46 -0400

    Testing from macbook1

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
-import java.util.*;
-class Sample
-{
+import java.util.ArrayList;
+
+public class Sample {
+
   public static void main(String args[]){
 
       System.out.println(" this is good");
@@ -8,7 +9,17 @@ class Sample
       LinkedList<String> list=new LinkedList<String>();
       list.add("Test");
       System.out.println("Linked List test, testing this"+list);
-      System.out.println(" This is sop on Macbook");
+      System.out.println(" This is sop on Macbook,testing");
+      ArrayList test = new ArrayList();
+      LinkedHashMap hashmap= new LinkedHashMap();
+      System.out.println(" This is good");
+      ArrayList<String> lis1=new ArrayList<String>();
+      lis1.add(new String("Test"));
+
+      for(int i=0;i<100;i++){
+
+      }
+
   }
 
 }
