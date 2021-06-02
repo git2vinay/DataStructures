f8e591e81198f801890f732e5678c4f1a8e2aa89
git2vinay <informvinay@gmail.com>
2021-06-01 22:48:44 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ class Sample
   public static void main(String args[]){
 
       System.out.println(" this is good");
-      System.out.println(" this is good");
+      System.out.println(" this is good,edited from the git");
   }
 
 }
